fc0c2f7ea13ef05e65df8d4b90db36209f9d238c
JuhoSoivio <101523348+JuhoSoivio@users.noreply.github.com>
2024-04-12 16:55:58 +0300

    Menus
    Menusystem created

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
