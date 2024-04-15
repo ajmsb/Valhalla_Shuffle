a2f3486d88de01738ace9c33b028be909a0344a1
Abdeljalil Misbah <aj.msb91@gmail.com>
2024-04-15 08:43:24 +0300

    small changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,39 +6,39 @@ public class GameManager : MonoBehaviour
 {
     [System.Serializable]
 
-    // Cards Class
-    public class Cards
+    // Card Class
+    public class Card
     {
         public int Id;
         public GameObject CardPiece;
 
         // Constructor
-        public Cards(int id, GameObject obj)
+        public Card(int id, GameObject obj)
         {
             Id = id;
             CardPiece = obj;
         }
     }
     // Create list of crads
-    public List<Cards> allCards = new List<Cards>();
+    public List<Card> allCards = new List<Card>();
 
     // Generating the grid
     public GameObject CardPrefab;
     public Vector2 Grid;
-    public void InitCards()
+    public void InitCard()
     {
         for (int x = 0; x < Grid.x; x++)
         {
             for (int y = 0; y < Grid.y; y++)
             {
                 GameObject NewCard = Instantiate(CardPrefab, new Vector3(x, y, 0), Quaternion.Euler(0, 0, 0), transform);
-                allCards.Add(new Cards(x + y, NewCard));
+                allCards.Add(new Card(x + y, NewCard));
             }
         }
     }
     void Start()
     {
-        InitCards();
+        InitCard();
     }
 
     void Update()
