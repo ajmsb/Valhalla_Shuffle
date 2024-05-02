8ba600657be119d39c226bcb30003145a5dcd926
JuhoSoivio <101523348+JuhoSoivio@users.noreply.github.com>
2024-05-02 14:54:59 +0300

    Timer
    Added a timer to level1

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class Menu : MonoBehaviour
 {
+    public static int level;
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
@@ -13,16 +15,19 @@ public class Menu : MonoBehaviour
     public void Level1()
     {
         SceneManager.LoadScene(2);
+        level= 1;
     }
 
     public void Level2()
     {
         SceneManager.LoadScene(3);
+        level= 2;
     }
 
     public void Level3()
     {
         SceneManager.LoadScene(4);
+        level= 3;
     }
 
     public void Rewards()
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public static TextMeshProUGUI timerText;
+    public static float timeValue;
+    bool timeStarted = false;
+
+    private void Start()
+    {
+        timerText = GameObject.Find("Timer").GetComponent<TextMeshProUGUI>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (timeValue <= 0)
+        {
+            timeValue = 0;
+        }
+
+        timeValue -= Time.deltaTime;
+
+        if (Menu.level == 1 && !timeStarted)
+        {
+            timeStarted = true;
+            timeValue = 30;
+            
+        }
+        timerText.text = "Time left: " + Mathf.Round(timeValue).ToString();
+
+        if (timeValue<=0 && timeStarted)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+
+    }
+
+
+
+
+
+}
